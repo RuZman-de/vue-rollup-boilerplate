8d80a0190bf6e9e7be4aece525068f32ef6f26ff
ruzmanz <zoltan.ruzman@msg.group>
2020-06-08 15:52:09 +0200

    remove variables

--- template/src/main.js
+++ template/src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
-{{#router}}import VueRouter from 'vue-router';{{/router}}
+import VueRouter from 'vue-router';
 import App from './App.vue';
-{{#if_eq vuetify "vuetify"}}import vuetify from './plugins/vuetify';{{/if_eq}}
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
-new Vue(\{{{#if_eq vuetify "vuetify"}}
-  }vuetify,{{/if_eq}}
+new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
