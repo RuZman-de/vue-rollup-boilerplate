34761db21f0bcf3ad03c0601e6a9f2949a722ba6
ruzmanz <zoltan.ruzman@msg.group>
2020-06-08 15:02:11 +0200

    fix vuetify

--- template/src/main.js
+++ template/src/main.js
@@ -1,15 +1,13 @@
 import Vue from 'vue';
-{{#router}}
 import VueRouter from 'vue-router';
-{{/router}}
 import App from './App.vue';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-{{#router}}
 Vue.use(VueRouter);
-{{/router}}
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
