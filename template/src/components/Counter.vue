8ab9d46d3c7a387226199bf9d5952905bc9097b0
ruzmanz <zoltan.ruzman@msg.group>
2020-06-09 15:54:52 +0200

    add vue.observables store

--- template/src/components/Counter.vue
+++ template/src/components/Counter.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
-    <p>{{counter}} </p>
+    <p>\{{counter}}</p>
     <v-btn icon @click="setCounter(counter + 1)"><v-icon>mdi-plus</v-icon></v-btn>
-    <v-btn icon @click="setCounter(counter - 1)"><v-icon>mdi-minus</v-icon></v-btn>   
-    <v-btn icon @click="resetCounter()"><v-icon left>mdi-delete</v-icon></v-btn>   
+    <v-btn icon @click="setCounter(counter - 1)"><v-icon>mdi-minus</v-icon></v-btn>
+    <v-btn icon @click="resetCounter()"><v-icon left>mdi-delete</v-icon></v-btn>
 </div>
 </template>
 
@@ -12,7 +12,7 @@ import { store, mutations } from "../store.js";
 
   export default {
     computed: {
-      counter() { return store.counter } 
+      counter() { return store.counter }
     },
      methods: {
       setCounter: mutations.setCounter,
